c7dea33cb7e1f299fdca8e183251d0758d38a9aa
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-08-25 21:11:05 +0100

    cors enabled

--- src/main/java/makarov/learning/controller/UserController.java
+++ src/main/java/makarov/learning/controller/UserController.java
@@ -3,6 +3,7 @@ package makarov.learning.controller;
 import makarov.learning.model.User;
 import makarov.learning.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="*") //on a class ok
 @RequestMapping("api/")
 public class UserController {
     @Autowired
--- src/main/java/makarov/learning/repository/UserRepository.java
+++ src/main/java/makarov/learning/repository/UserRepository.java
@@ -4,10 +4,12 @@ import makarov.learning.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @Repository
+@CrossOrigin(origins="")
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByFirstName(@Param("firstName") String firstName);
     // -> http://localhost:8080/rest/users/search/findByFirstName?firstName=MiroBuilder
