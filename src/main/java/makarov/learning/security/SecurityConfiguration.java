b894edf0ab62828a9d316e38ea57eff09dd78d21
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-09-02 19:26:57 +0100

    before deleting my original user schemas

--- src/main/java/makarov/learning/security/SecurityConfiguration.java
+++ src/main/java/makarov/learning/security/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,13 +16,23 @@ import javax.sql.DataSource;
 
 // @EnableWebSecurity //to enable WEB security
 @Configuration
-// public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+// public class SecurityConfiguration {
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return new MyUserDetailsService_LoadUserByUsername();
+
+    @Autowired DataSource dataSource;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                ;
     }
+
+    // @Bean
+    // public UserDetailsService userDetailsService(){
+    //     return new MyUserDetailsService_LoadUserByUsername();
+    // }
 
     // @Autowired DataSource dataSource;
     // @Bean
